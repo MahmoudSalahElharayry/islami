f42252fb427821049acef3bfeedabd1daeb4eb17
Elharayry <elharayry123@gmail.com>
2021-10-25 08:57:53 +0200

    Added the Bottom Navigation Bar

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,12 +1,52 @@
 import 'package:flutter/material.dart';
 
+import 'main.dart';
+
 class HomePage extends StatelessWidget {
   static const routeName = 'home';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(),
+    return Stack(
+      children: [
+        Image.asset(
+          'assets/images/main_background.png',
+          width: double.infinity,
+          height: double.infinity,
+          fit: BoxFit.fill,
+        ),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          bottomNavigationBar: Theme(
+            data: Theme.of(context).copyWith(
+              canvasColor: MyThemeData.primaryColor,
+            ),
+            child: BottomNavigationBar(
+              currentIndex: 0,
+              selectedItemColor: MyThemeData.SelectedIconColor,
+              unselectedItemColor: MyThemeData.UnSelectedIconColor,
+              backgroundColor: MyThemeData.primaryColor,
+              items: [
+                BottomNavigationBarItem(
+                    label: 'Quran',
+                    icon: Image.asset(
+                      'assets/images/ic_quran.png',
+                      width: 36,
+                    )),
+                BottomNavigationBarItem(
+                    label: 'ahadeth',
+                    icon: Image.asset('assets/images/ic_book.png', width: 36)),
+                BottomNavigationBarItem(
+                    label: 'Sebha',
+                    icon: Image.asset('assets/images/ic_sebha.png', width: 36)),
+                BottomNavigationBarItem(
+                    label: 'Radio',
+                    icon: Image.asset('assets/images/ic_radio.png', width: 36)),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,18 @@ void main() {
   runApp(MyApp());
 }
 
+class MyThemeData {
+  static final primaryColor = Color.fromRGBO(183, 147, 95, 1.0);
+  static final SelectedIconColor = Color.fromRGBO(0, 0, 0, 1.0);
+  static final UnSelectedIconColor = Color.fromRGBO(255, 255, 255, 1.0);
+}
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      theme: ThemeData(primaryColor: MyThemeData.primaryColor),
       routes: {HomePage.routeName: (buildContext) => HomePage()},
       initialRoute: HomePage.routeName,
     );
